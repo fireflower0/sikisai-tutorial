8c7decf60a6b124a51b003d2d2d7575a42137c9c
fireflower0 <fireflower0@hotmail.co.jp>
2019-01-21 11:04:08 +0900

    #7 Add a draw text program

---
+++ 00/draw-text.lisp
@@ -0,0 +1,18 @@
+(defpackage sikisai-tutorial/00/draw-text
+  (:use :cl)
+  (:import-from :sikisai)
+  (:export :main))
+(in-package :sikisai-tutorial/00/draw-text)
+
+(defparameter *width*  480)
+(defparameter *height* 380)
+
+(defclass window (sik:window) ())
+
+(defmethod sik:user-display ((this window))
+  (sik:texts "Sikisai Tutorial" 200.0 200.0))
+
+(defun main ()
+  (sik:display-window (make-instance 'window
+                                     :width *width*
+                                     :height *height*)))
--- app.lisp
+++ app.lisp
@@ -1,11 +1,15 @@
 (defpackage :sikisai-tutorial/app
   (:use :cl)
   (:import-from :sikisai-tutorial/00/simple-window)
-  (:import-from :sikisai-tutorial/00/simple-window2))
+  (:import-from :sikisai-tutorial/00/simple-window2)
+  (:import-from :sikisai-tutorial/00/draw-text))
 (in-package :sikisai-tutorial/app)
 
 ;; 00 Simple Window
 ;; (sikisai-tutorial/00/simple-window:main)
 
 ;; 00 Simple Window 2
-(sikisai-tutorial/00/simple-window2:main)
+;; (sikisai-tutorial/00/simple-window2:main)
+
+;; 00 Draw Text
+;; (sikisai-tutorial/00/draw-text:main)
